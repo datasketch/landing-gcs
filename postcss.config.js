24f77cbde1f9be73cc7b938a3f42b7242e52444c
David Daza <03dazal@gmail.com>
2019-12-05 11:50:33 -0500

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./layouts/**/*.html'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss,
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'gable-green': '#173637',
+        'porcelain': '#f0f4f5',
+        'pickled-bluewood': '#2e4856',
+        'celery': '#b0d361',
+        'pesto': '#637f36',
+        'mantis': '#94ce5b',
+        'nevada': '#5e6f77',
+      },
+      fontFamily: {
+        'barlow': ['Barlow', 'sans-serif'],
+        'barlow-condensed': ['"Barlow Condensed"', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
